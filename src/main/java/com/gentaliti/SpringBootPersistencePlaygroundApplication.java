f0bf7d8c23d059da8ad40ebeab4a19773f2af919
Gent Aliti <aliti.genti@gmail.com>
2020-10-11 13:31:13 +0200

    Initial commit

---
+++ src/main/java/com/gentaliti/SpringBootPersistencePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.gentaliti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPersistencePlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPersistencePlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gentaliti/SpringBootPersistencePlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gentaliti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPersistencePlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
